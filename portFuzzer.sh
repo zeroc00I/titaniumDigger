43d7fd576a3f1ef156ee0c893b17ae8e8ce32d76
Bruno Minossi <b.minossi@gmail.com>
2021-02-06 00:08:52 -0300

    Fixing identation

--- portFuzzer.sh
+++ portFuzzer.sh
@@ -2,25 +2,30 @@
 
 GREEN=`tput setaf 2`
 
-portByOpenFrequency=`sort -r -k3 /usr/share/nmap/nmap-services | tr '/' ' ' | awk '!/^#/{print $2}'`
+portByOpenFrequency=`
+	sort -r -k3 /usr/share/nmap/nmap-services |
+	tr '/' ' ' |
+	awk '!/^#/{print $2}'`
 allHosts="$1"
 
 cat $allHosts | 
 xargs -P400 -I@ sh -c '
-ip=$(dig +short @); 
-[ ! -z "$ip" ]] &&
-[ ! -z "${ip##*52.*}" ] &&
-[ ! -z "${ip##*54.*}" ] && 
-[ ! -z "${ip##*18.*}" ] && 
-[ ! -z "${ip##10.*}" ] && 
-echo "$ip" | grep -vE "[a-zA-Z]" | anew ipsUp
-'
+	ip=$(dig +short @); 
+	[ ! -z "$ip" ]] &&
+	[ ! -z "${ip##*52.*}" ] &&
+	[ ! -z "${ip##*54.*}" ] && 
+	[ ! -z "${ip##*18.*}" ] && 
+	[ ! -z "${ip##10.*}" ] && 
+	echo "$ip" | grep -vE "[a-zA-Z]" | anew ipsUp'
 
 echo "Starting Scan using IpsUp File"
 
 echo "$portByOpenFrequency" |
 while read line;
 	do echo "$line" |
-	         xargs -a "$allHosts" -P 500 -I@ sh -c "timeout 1 bash -c '>/dev/tcp/@/$line' 2>/dev/null && echo ${GREEN}[OPEN] @ $line";
+		xargs -a "$allHosts" -P 500 -I@ sh -c "
+		         timeout 1 bash -c '
+		         >/dev/tcp/@/$line
+		         ' 2>/dev/null && echo ${GREEN}[OPEN] @ $line";
 	done |
     anew openPorts
