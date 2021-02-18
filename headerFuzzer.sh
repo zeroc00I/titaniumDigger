d8555c2a02afb1f07d4eed48cc735bdab6f6df2e
Bruno Minossi <b.minossi@gmail.com>
2021-02-18 01:54:04 -0300

    HttpFuzzer Pure Bash

--- headerFuzzer.sh
+++ headerFuzzer.sh
@@ -2,7 +2,8 @@
 
 set -e 
 formatHeaderListFuzzer(){
-	headersWithReflectedValues=$(awk '{x=NR+1}(NR<=x){print $0": z3r0c00I"NR }' /opt/SecLists/Discovery/Web-Content/BurpSuite-ParamMiner/lowercase-headers)
+	headersWithReflectedValues=$(
+	awk '{x=NR+1}(NR<=x){print $0": z3r0c00I"NR }' "$1" )
 	echo -e "$headersWithReflectedValues" 
 }
 
@@ -18,7 +19,9 @@ parallelFuzzerWithMaxHeader(){
 }
 
 main(){
-	allHeaders=$(formatHeaderListFuzzer)
+	#should be recieved by arg parser
+	wordList='/opt/SecLists/Discovery/Web-Content/BurpSuite-ParamMiner/lowercase-headers'
+	allHeaders=$(formatHeaderListFuzzer "$wordList")
 	result=$(buildRequestWithNHeaders "$allHeaders" "10")
 	echo -e "$result"
 }
