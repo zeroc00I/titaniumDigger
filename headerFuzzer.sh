f05544a2c55118a64da6d37225a32069aa7acd4a
Bruno Minossi <b.minossi@gmail.com>
2021-02-18 03:18:31 -0300

    header fuzzer v1

--- headerFuzzer.sh
+++ headerFuzzer.sh
@@ -8,22 +8,50 @@ formatHeaderListFuzzer(){
 }
 
 buildRequestWithNHeaders(){
+	# $1 all keyPairs
+	# $2 max n headers / req
+	# $3 target domain
+	# $4 req timeout
+
 	echo -e "$1" |
 	awk '{x=NR+1}(NR<=x){print $0}(NR%'$2'==0){print "#"}' |
-	xargs -P100 -I@ -d '#' sh -c 'echo "@"'
+	xargs -P100 -I@ -d '#' bash -c 'headerKeyPairToCurlFormat "@" "'$3'" '$4
+}
+
+headerKeyPairToCurlFormat(){
+	# $1 all keyPairs
+	# $2 target domain
+	# $3 request timeout
+
+	echo -e "$1" | 
+	sed 's/^/-H "/g;s/$/"/g' | 
+	tr '\n' ' ' | 
+	sed 's#^#\ncurl "'$2'" -s -m "'$3'" #g;s/-H ""//g'
 }
 
 parallelFuzzerWithMaxHeader(){
-	echo "$1" |
-	xargs -P "$2" sh -c '' 
+	fuzzResult=$(
+		echo -e "$1" |
+		xargs -0 -P "$2" -I@ sh -c '@'
+	)
+	scanReflectedHeaders "$fuzzResult"
+}
+
+scanReflectedHeaders(){
+	valueReflected=$(echo -e "$1" | grep -Eo 'z3r0c00I[[:digit:]]{1,}')
+	keyReflected=$(echo -e $allHeaders | grep -oE "[a-zA-Z0-9-]{1,}\:.$valueReflected")	
+	echo -e "$keyReflected"
 }
 
 main(){
 	#should be recieved by arg parser
 	wordList='/opt/SecLists/Discovery/Web-Content/BurpSuite-ParamMiner/lowercase-headers'
+	export -f headerKeyPairToCurlFormat
+	export -f scanReflectedHeaders
+
 	allHeaders=$(formatHeaderListFuzzer "$wordList")
-	result=$(buildRequestWithNHeaders "$allHeaders" "10")
-	echo -e "$result"
+	headerKeyPair=$(buildRequestWithNHeaders "$allHeaders" "80" 'https://iam.zerocool.cf/headervul.php' "2")
+	parallelFuzzerWithMaxHeader "$headerKeyPair" "2"
 }
 
 main
