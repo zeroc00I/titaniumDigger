1e16382e14450f397d62cf85a3bb20a5902cfcb8
jaderkb@gmail.com <jaderkb@gmail.com>
2021-02-19 23:01:59 -0300

    changing argvs func

--- headerFuzzer.py
+++ headerFuzzer.py
@@ -1,30 +1,40 @@
 import getopt, sys
+   
+wordList = []
 
 
-
-# Get full command-line arguments
-full_cmd_arguments = sys.argv
-
-# Keep all but the first
-argument_list = full_cmd_arguments[1:]
-
-short_options = "hv"
-long_options = ["help", "verbose"]
-
-try:
-    arguments, values = getopt.getopt(argument_list, short_options, long_options)
-except getopt.error as err:
-    # Output error, and return with an error code
-    print (str(err))
-    sys.exit(2)
+def loadWordlist(wordlistPath):
+    with open(wordlistPath) as listPayloads:
+            for line in listPayloads:
+                wordList.append(line)
+            for i in wordList:
+                print(i)
 
 
-for current_argument, current_value in arguments:
-    if current_argument in ("-v", "--verbose"):
-        print ("Enabling verbose mode")
-    elif current_argument in ("-h", "--help"):
-        print('headerFuzzer [--help] [-d|--domain] [-w|--wordlist]')
-        print ('-c: Max curl workers')
-        print ('-m: Request Time Out limit')
-        print ('-r: Max request header per request')
+def main():
+    isH = True
+    full_cmd_arguments = sys.argv # Get full command-line arguments
+    argument_list = full_cmd_arguments[1:] # Keep all but the first
+    short_options = "hvw:d"
+    try:
+        arguments, argv = getopt.getopt(argument_list, short_options, ["w="])
+    except getopt.error as err: # Output error, and return with an error code
+        print (str(err))
+        sys.exit(2)
+    for current_argument in arguments:
+        print(current_argument)
+        if ("-h" in current_argument) or ("-help" in current_argument):
+            print('headerFuzzer [--help] [-d|--domain] [-w|--wordlist]')
+            print ('-c: Max curl workers')
+            print ('-m: Request Time Out limit')
+            print ('-r: Max request header per request')
+            isH = False
+    if (isH == True):
+        for current_argument, current_value in arguments:
+            if ("-v" in current_argument)  or ("-verbose" in current_argument):
+                print ("Enabling verbose mode")
+            elif ("-w" in current_argument) or ("-wordlist" in current_argument):
+                loadWordlist(current_value)
 
+if __name__ == "__main__":
+    main()
