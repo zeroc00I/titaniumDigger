5215eec4edc9a7b6f2703286ca969dd0c8e4827a
bminossi <root@vmi481414.contaboserver.net>
2021-03-06 21:51:21 -0600

    new version with argsparse

--- pocXss.py
+++ pocXss.py
@@ -1,50 +1,27 @@
 import getopt, sys
 import requests
-   
+import argparse
+import os
+
 argumentList = []
 urlList = []
 
 def loadArgumentList(argumentListPath):
-    with open(argumentListPath) as argumentFileList:
-            for line in argumentFileList:
+            for line in argumentListPath.readlines():
                 lineArgumentWithoutN = line.replace('\n','')
                 argumentList.append(lineArgumentWithoutN)
 
 
-def loadURrlList(argumentListPath):
-    with open(argumentListPath) as urlFileList:
-            for line in urlFileList:
-                lineFileWithoutN = line.replace('\n','')
-                urlList.append(lineFileWithoutN)
-
-def main():
-    isH = True
-    full_cmd_arguments = sys.argv # Get full command-line arguments
-    argument_list = full_cmd_arguments[1:] # Keep all but the first
-    short_options = "hu:da:d"
-    try:
-        arguments, argv = getopt.getopt(argument_list, short_options, ["w="])
-    except getopt.error as err: # Output error, and return with an error code
-        print (str(err))
-        sys.exit(2)
-    for current_argument in arguments:
-        if ("-h" in current_argument) or ("-help" in current_argument):
-            print('headerFuzzer [--help] [-d|--domain] [-w|--wordlist]')
-            print ('-m: Request Time Out limit')
-            print ('-r: Max request header per request')
-            isH = False
-    if (isH == True):
-        for current_argument, current_value in arguments:
-            if ("-a" in current_argument) or ("--argument" in current_argument):
-                loadArgumentList(current_value)
-            if ("-u" in current_argument) or ("--url" in current_argument):
-                loadURrlList(current_value) 
-        definitiveList = makeDefinitiveList(urlList, argument_list)       
-        makeRequest(definitiveList)
+def loadURLList(urlListPath):
+    for line in urlListPath.readlines():
+        lineFileWithoutN = line.replace('\n','')
+        urlList.append(lineFileWithoutN)
 
 def makeDefinitiveList(urlList, argument_list):
     definitiveList = []
+    print("lista",urlList) 
     for urlEntry in urlList:
+        print(urlEntry)
         for argumentEntry in argumentList:
             definitiveList.append(urlEntry+"?"+argumentEntry+"=198522355kkll")
     return definitiveList
@@ -61,8 +38,34 @@ def makeRequest(definitiveList):
         if ("198522355kkll" in response.text):
             print("Refletido em:"+url)
         else:
-            print("n achou")     
+            print("Não achou para %s" % url)     
 
+def usage():
+    print('headerFuzzer [--help] [-d|--domain] [-w|--wordlist]')
+    print ('-m: Request Time Out limit')
+    print ('-r: Max request header per request')
+    exit()
+
+def menu():
+
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('-m', help='Max Seconds Request timeout', dest='maxReqTimeout', type=int)
+    parser.add_argument('-r', help='Max request header per request', dest='maxHeaderReq', type=int)
+    parser.add_argument('-w', help='Payloads from wordlist file', dest='payloadWordlist', type=argparse.FileType('r'))
+    parser.add_argument('-u', help='Target single url', dest='url')
+    parser.add_argument('-U', help='Target multiple url from file', dest='multipleUrl', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    
+    return args
+
+def main():
+
+    args = menu()
+    loadArgumentList(args.payloadWordlist)
+    loadURLList(args.multipleUrl) 
+    
+    definitiveList = makeDefinitiveList(urlList, argumentList) 
+    makeRequest(definitiveList)
 
 if __name__ == "__main__":
     main()
