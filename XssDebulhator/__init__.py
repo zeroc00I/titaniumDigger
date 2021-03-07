0ff9552763d99b58f10abe53f5fb1ac20e8dcdac
jaderkb@gmail.com <jaderkb@gmail.com>
2021-03-07 01:54:03 -0300

    Adicionado lista STD

--- XssDebulhator/__init__.py
+++ XssDebulhator/__init__.py
@@ -1,9 +1,14 @@
 import requests
 import argparse
-import os
-
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from XssDebulhator.module import StdArgument
+stdArgumentList = StdArgument.stdArgument()
 argumentList = []
 urlList = []
+#tdArgumentList = stdArgument
 
 def loadArgumentList(argumentListPath):
     for line in argumentListPath.readlines():
---
+++ XssDebulhator/module/StdArgument.py
@@ -0,0 +1,9 @@
+
+def stdArgument():
+    stdArgument = []
+    stdArgument.append('id')
+    stdArgument.append('teste')
+    stdArgument.append('test')
+    stdArgument.append('p')
+    stdArgument.append('url')
+    return stdArgument
