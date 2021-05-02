73e47d64100ce9be7fdc9f741597825f8bbb5fb7
root <root@vmi481414.contaboserver.net>
2021-05-02 00:58:17 -0500

    self xss hunter grabbin js vars

---
+++ varsandforms.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+NON=`tput sgr0`
+#set -x
+
+function fire(){
+payload=$(curl -sfkL "$line"| tr ';' '\n' | grep -Eo 'var.*[^=]*=|name=".*[^"]* ' | tr '="' ' ' | awk '{print $2}' | sort -u| sed -z 's#\n#\=zeroc00I"\&#g' | sed 's#^#/?#g' | sed "s#^#$line#g")
+if [ ! -z "$payload" ]; then
+ echo "$YELLOW[Requesting]$NON $payload"	
+ curl -sfLk "$payload" | grep -q 'zeroc00I"' && echo "$GREEN[Reflected]$NON $payload"
+else
+ echo "$RED[Failed]$NON $line"
+fi
+}
+
+
+if [ -p /dev/stdin ]; then
+ checkThisUrl=$(</dev/stdin)
+ echo "$checkThisUrl" | tr ' ' '\n' | while read line;do fire "$line";done
+else
+ echo "Single Thread"
+ checkThisUrl="$1"
+ fire "$checkThisUrl"
+fi
