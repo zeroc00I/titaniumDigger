9da4d9ceaaca58c4b49ea2ea9e01dd234ef04137
Bruno - Zeroc00I <b.minossi@gmail.com>
2023-10-12 02:11:28 -0300

    Update net_monitor_to_db.go

--- net_monitor_to_db.go
+++ net_monitor_to_db.go
@@ -1,92 +1,256 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"net"
-	"os"
-	"time"
+    "database/sql"
+    "fmt"
+    "html/template"
+    "net"
+    "net/http"
+    "os"
+    "strings"
+    "sync"
+    "time"
 
-	_ "github.com/mattn/go-sqlite3"
+    "github.com/gorilla/mux"
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/microcosm-cc/bluemonday"
+    "encoding/json"
 )
 
-func handleClient(conn net.Conn, db *sql.DB) {
-	defer conn.Close()
+var (
+    db           *sql.DB
+    templateText = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>SQLite Data Table</title>
+    <style>
+            body {
+                font-family: Arial, sans-serif;
+            }
+    
+            h1 {
+                color: #333;
+            }
+    
+            table {
+                width: 100%;
+table-layout: fixed;
+                max-width: 100%;
+                border-collapse: collapse;
+                margin: 20px 0;
+            }
+    
+            table, th, td {
+                border: 1px solid #333;
+            }
+    
+            th, td {
+                padding: 8px;
+                text-align: left;
+                overflow: hidden;
+                white-space: nowrap;
+            }
+    
+            th {
+                background-color: #333;
+                color: #fff;
+            }
+            td:nth-child(3) {
+        white-space: normal; /* Allow text to wrap to the next line */
+    }
+            tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
+        </style>
+	</head>
+    <body>
+        <h1>SQLite Data Table</h1>
+        <table id="data-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>IP</th>
+                    <th>Content</th>
+                    <th>Port Number</th>
+                    <th>Datetime</th>
+                </tr>
+            </thead>
+            <tbody id="table-body">
+                {{range .}}
+                <tr>
+                    <td>{{.ID}}</td>
+                    <td>{{.IP}}</td>
+                    <td>{{.Content | html}}</td>
+                    <td>{{.Port}}</td>
+                    <td>{{.Datetime}}</td>
+                </tr>
+                {{end}}
+            </tbody>
+        </table>
+        <script>
+            function updateTable() {
+                fetch("/data")
+                    .then(response => response.json())
+                    .then(data => {
+                        const table = document.getElementById("data-table");
+                        const tbody = document.getElementById("table-body");
+    
+                        // Clear the existing rows
+                        tbody.innerHTML = "";
+    
+                        // Iterate through the data in reverse order (latest first)
+                        for (let i = data.length - 1; i >= 0; i--) {
+                            const row = data[i];
+                            const tr = document.createElement("tr");
+                            tr.innerHTML = "<td>" + row.ID + "</td><td>" + row.IP + "</td><td>" + row.Content + "</td><td>" + row.Port + "</td><td>" + row.Datetime + "</td>";
+                            tbody.appendChild(tr);
+                        }
+                    });
+            }
+            updateTable(); // Initial update
+            setInterval(updateTable, 3000); // Update every 3 seconds
+        </script>
+    </body>
+    </html>
+    `
+)
 
-	clientAddress := conn.RemoteAddr()
-	fmt.Printf("Connected to client from IP address: %s\n", clientAddress)
-
-	response := "HTTP/2 200\r\n\r\n"
-	_, err := conn.Write([]byte(response))
-	if err != nil {
-		fmt.Printf("Error sending response to client: %s\n", err)
-		return
-	}
-
-	buffer := make([]byte, 1024)
-	for {
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Printf("Error reading from client: %s\n", err)
-			return
-		}
-
-		data := buffer[:n]
-		fmt.Printf("Received data from client: %s\n", string(data))
-
-		// Store data in the local database
-		storeDataInDatabase(clientAddress.String(), string(data), db)
-	}
+type Data struct {
+    ID       int
+    IP       string
+    Content  string
+    Port     string
+    Datetime string
 }
 
-func storeDataInDatabase(ip, content string, db *sql.DB) {
-	stmt, err := db.Prepare("INSERT INTO data (ip, content, datetime) values (?, ?, ?)")
-	if err != nil {
-		fmt.Printf("Error preparing SQL statement: %s\n", err)
-		return
-	}
-	defer stmt.Close()
+func getData(w http.ResponseWriter, r *http.Request) {
+    var data []Data
+    rows, err := db.Query("SELECT * FROM data")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
 
-	_, err = stmt.Exec(ip, content, time.Now())
-	if err != nil {
-		fmt.Printf("Error inserting data into the database: %s\n", err)
-	}
+    for rows.Next() {
+        var d Data
+        err = rows.Scan(&d.ID, &d.IP, &d.Content, &d.Port, &d.Datetime)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        data = append(data, d)
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    if err := json.NewEncoder(w).Encode(data); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func main() {
-	host := "0.0.0.0"
-	port := 65535
+    // Open the SQLite database
+    dbPath := "local.db"
+    var err error
+    db, err = sql.Open("sqlite3", dbPath)
+    if err != nil {
+        fmt.Printf("Error opening the database: %s\n", err)
+        os.Exit(1)
+    }
+    defer db.Close()
 
-	// Open the SQLite database
-	db, err := sql.Open("sqlite3", "local.db")
-	if err != nil {
-		fmt.Printf("Error opening the database: %s\n", err)
-		os.Exit(1)
-	}
-	defer db.Close()
+    // Create the data table if it doesn't exist
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, ip TEXT, content TEXT, port TEXT, datetime DATETIME)")
+    if err != nil {
+        fmt.Printf("Error creating the data table: %s\n", err)
+        os.Exit(1)
+    }
 
-	// Create the data table if it doesn't exist
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, ip TEXT, content TEXT, datetime DATETIME)")
-	if err != nil {
-		fmt.Printf("Error creating the data table: %s\n", err)
-		os.Exit(1)
-	}
+    // Set up the web server
+    r := mux.NewRouter()
+    r.HandleFunc("/data", getData)
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        tmpl, err := template.New("webpage").Parse(templateText)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        if err := tmpl.Execute(w, nil); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    })
 
-	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
-	if err != nil {
-		fmt.Printf("Error binding to port %d: %s\n", port, err)
-		os.Exit(1)
-	}
+    srv := &http.Server{
+        Handler:      r,
+	Addr:         "0.0.0.0:12",
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
 
-	fmt.Printf("Listening on port %d...\n", port)
+    go func() {
+        if err := srv.ListenAndServe(); err != nil {
+            fmt.Printf("HTTP server stopped: %s\n", err)
+        }
+    }()
+    // ...
+    fmt.Println("Web server is running on http://localhost:8080")
 
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			fmt.Printf("Error accepting connection: %s\n", err)
-			continue
-		}
+    // Start the server listening on port 65535
+    listener, err := net.Listen("tcp", "0.0.0.0:65535")
+    if err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+        os.Exit(1)
+    }
+    defer listener.Close()
 
-		go handleClient(conn, db)
-	}
+    var wg sync.WaitGroup // WaitGroup to manage Goroutines
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Printf("Error accepting connection: %s\n", err)
+            continue
+        }
+        wg.Add(1)
+        go handleConnection(conn, db, &wg) // Handle each connection concurrently
+    }
+
+    select {}
+}
+
+func handleConnection(conn net.Conn, db *sql.DB, wg *sync.WaitGroup) {
+    defer wg.Done()
+    defer conn.Close()
+
+    // Read data from the connection
+    buffer := make([]byte, 1024)
+    n, err := conn.Read(buffer)
+    if err != nil {
+        fmt.Printf("Error reading data: %s\n", err)
+        return
+    }
+    data := string(buffer[:n])
+
+    // Extract the client's IP address and port number
+    remoteAddr := conn.RemoteAddr().String()
+    parts := strings.Split(remoteAddr, ":")
+    var port string
+    if len(parts) > 1 {
+        port = parts[len(parts)-1]
+    } else {
+        port = "N/A"
+    }
+
+    // Sanitize the HTML content using bluemonday
+    sanitizer := bluemonday.UGCPolicy()
+    sanitizedData := sanitizer.Sanitize(data)
+
+    // Insert data, IP, sanitized content, and port into the database
+    _, err = db.Exec("INSERT INTO data (ip, content, port, datetime) VALUES (?, ?, ?, datetime('now'))", remoteAddr, sanitizedData, port)
+    if err != nil {
+        fmt.Printf("Error inserting data into the database: %s\n", err)
+    }
 }
